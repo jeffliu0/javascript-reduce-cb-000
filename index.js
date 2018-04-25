365b76f59ec2d43b1bf43c7d61a4a3480ed66442
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-25 05:02:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much."
+
+const products = [
+  { name: 'Head & Shoulders Shampoo', price: 4.99, discount: .6 },
+  { name: 'Twinkies', price: 7.99, discount: .45 },
+  { name: 'Oreos', price: 6.49, discount: .8 },
+  { name: 'Jasmine-scented bath pearls', price: 13.99, discount: .7 }
+];
+
+const reducer = (acc, product) => {
+  acc + (product.price * product.discount)
+}
+
+const reduced = products.reduce(reducer, 0)
